155060c3fe9d1530dfca6601886cdafdc3fcc52a
bineetNaidu <bineet.s.naidu@gmail.com>
2020-09-27 13:22:24 +0530

    add base Header && styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
+      {/* Header */}
+      <div className="app__header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram Logo"
+          className="app__headerImage"
+        />
+      </div>
+
       <h1>Yo!! Lets build a Instagram Clone</h1>
+      {/* Posts */}
     </div>
   );
 }
